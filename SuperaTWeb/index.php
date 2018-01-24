2a7b398c3362e2136edf9f2b3d2af9b411bee259
luis_david <ldgarciabernal@gmail.com>
2018-01-24 12:25:50 +0100

    Superta Completo + WebSite

--- SuperaTWeb/index.php
+++ SuperaTWeb/index.php
@@ -17,11 +17,105 @@
 			if (!$connect) {
 				echo "ERROR";
 			} else {
-				$sql = "SELECT * FROM description";
+				$sql = "SELECT * FROM description ORDER BY date;";
 				$result = mysqli_query($connect, $sql);
 
 				if(mysqli_num_rows($result) > 0){
-					echo "200";
+					
+					echo '
+					<table width="70%" border="1px" align="center">
+
+					<tr align="center">
+				        <td>Date</td>
+				        <td>DNI</td>
+				        <td>Fobia</td>
+				        <td>Max</td>
+				        <td>Min</td>
+				        <td>Difference</td>
+				        <td>seconds 01</td>
+				        <td>seconds 02</td>
+				        <td>seconds 03</td>
+				        <td>seconds 04</td>
+				        <td>seconds 05</td>
+				        <td>seconds 06</td>
+				        <td>seconds 07</td>
+				        <td>seconds 08</td>
+				        <td>seconds 09</td>
+				        <td>seconds 10</td>
+				        <td>seconds 11</td>
+				        <td>seconds 12</td>
+				        <td>seconds 13</td>
+				        <td>seconds 14</td>
+				        <td>seconds 15</td>
+				        <td>seconds 16</td>
+				        <td>seconds 17</td>
+				        <td>seconds 18</td>
+				        <td>seconds 19</td>
+				        <td>seconds 20</td>
+				        <td>seconds 21</td>
+				        <td>seconds 22</td>
+				        <td>seconds 23</td>
+				        <td>seconds 24</td>
+				        <td>seconds 25</td>
+				        <td>seconds 26</td>
+				        <td>seconds 27</td>
+				        <td>seconds 28</td>
+				        <td>seconds 29</td>
+				        <td>seconds 30</td>
+				        <td>Time</td>
+			    	</tr>';
+
+					$i=0;
+					while ($row = mysqli_fetch_assoc($result)) {
+						
+						if ($i%2 == 0){
+							echo '<tr bgcolor="#819FF7">';
+						}else {
+							echo '<tr>';
+						}
+							
+							echo '<td>'.$row['date'].'</td>';
+							echo '<td>'.$row['dni_pacient'].'</td>';
+							echo '<td>'.$row['phobia'].'</td>';
+							echo '<td>'.$row['max'].'</td>';
+							echo '<td>'.$row['min'].'</td>';
+							echo '<td>'.$row['deff_max_min'].'</td>';
+							echo '<td>'.$row['s_01'].'</td>';
+							echo '<td>'.$row['s_02'].'</td>';
+							echo '<td>'.$row['s_03'].'</td>';
+							echo '<td>'.$row['s_04'].'</td>';
+							echo '<td>'.$row['s_05'].'</td>';
+							echo '<td>'.$row['s_06'].'</td>';
+							echo '<td>'.$row['s_07'].'</td>';
+							echo '<td>'.$row['s_08'].'</td>';
+							echo '<td>'.$row['s_09'].'</td>';
+							echo '<td>'.$row['s_10'].'</td>';
+							echo '<td>'.$row['s_11'].'</td>';
+							echo '<td>'.$row['s_12'].'</td>';
+							echo '<td>'.$row['s_13'].'</td>';
+							echo '<td>'.$row['s_14'].'</td>';
+							echo '<td>'.$row['s_15'].'</td>';
+							echo '<td>'.$row['s_16'].'</td>';
+							echo '<td>'.$row['s_17'].'</td>';
+							echo '<td>'.$row['s_18'].'</td>';
+							echo '<td>'.$row['s_19'].'</td>';
+							echo '<td>'.$row['s_20'].'</td>';
+							echo '<td>'.$row['s_21'].'</td>';
+							echo '<td>'.$row['s_22'].'</td>';
+							echo '<td>'.$row['s_23'].'</td>';
+							echo '<td>'.$row['s_24'].'</td>';
+							echo '<td>'.$row['s_25'].'</td>';
+							echo '<td>'.$row['s_26'].'</td>';
+							echo '<td>'.$row['s_27'].'</td>';
+							echo '<td>'.$row['s_28'].'</td>';
+							echo '<td>'.$row['s_29'].'</td>';
+							echo '<td>'.$row['s_30'].'</td>';
+							echo '<td>'.$row['time'].'</td>';
+						echo '</tr>';
+						$i++;
+					}
+					
+
 				} else { echo "401";}
 			}
 		?>
--- SuperaTWeb/superat.sql
+++ SuperaTWeb/superat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 18-01-2018 a las 11:15:05
+-- Tiempo de generación: 24-01-2018 a las 10:53:26
 -- Versión del servidor: 5.7.19
 -- Versión de PHP: 7.1.9
 
@@ -32,18 +32,64 @@ DROP TABLE IF EXISTS `description`;
 CREATE TABLE IF NOT EXISTS `description` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
+  `id_file` varchar(64) COLLATE utf8_bin NOT NULL,
   `date` date NOT NULL,
   `time` date NOT NULL,
   `max` float NOT NULL,
   `min` float NOT NULL,
   `deff_max_min` float NOT NULL,
-  `s_1` float NOT NULL,
-  `s_2` float NOT NULL,
-  `s_3` float NOT NULL,
-  `s_4` float NOT NULL,
+  `s_01` float NOT NULL,
+  `s_02` float NOT NULL,
+  `s_03` float NOT NULL,
+  `s_04` float NOT NULL,
+  `s_05` float NOT NULL,
+  `s_06` float NOT NULL,
+  `s_07` float NOT NULL,
+  `s_08` float NOT NULL,
+  `s_09` float NOT NULL,
+  `s_10` float NOT NULL,
+  `s_11` float NOT NULL,
+  `s_12` float NOT NULL,
+  `s_13` float NOT NULL,
+  `s_14` float NOT NULL,
+  `s_15` float NOT NULL,
+  `s_16` float NOT NULL,
+  `s_17` float NOT NULL,
+  `s_18` float NOT NULL,
+  `s_19` float NOT NULL,
+  `s_20` float NOT NULL,
+  `s_21` float NOT NULL,
+  `s_22` float NOT NULL,
+  `s_23` float NOT NULL,
+  `s_24` float NOT NULL,
+  `s_25` float NOT NULL,
+  `s_26` float NOT NULL,
+  `s_27` float NOT NULL,
+  `s_28` float NOT NULL,
+  `s_29` float NOT NULL,
+  `s_30` float NOT NULL,
   PRIMARY KEY (`Id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `description`
+--
+
+INSERT INTO `description` (`Id`, `user_id`, `id_file`, `date`, `time`, `max`, `min`, `deff_max_min`, `s_01`, `s_02`, `s_03`, `s_04`, `s_05`, `s_06`, `s_07`, `s_08`, `s_09`, `s_10`, `s_11`, `s_12`, `s_13`, `s_14`, `s_15`, `s_16`, `s_17`, `s_18`, `s_19`, `s_20`, `s_21`, `s_22`, `s_23`, `s_24`, `s_25`, `s_26`, `s_27`, `s_28`, `s_29`, `s_30`) VALUES
+(1, 1, 'dfdsfd113iebn', '2018-01-18', '2000-02-12', 10, 45, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 100, 110, 110, 110, 113, 200, 223, 123, 457, 342),
+(2, 1, 'dfdsfd113iebn', '2018-01-18', '2000-02-12', 565, 30, 545, 100, 110, 321, 110, 234, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 120, 110, 110, 343, 113, 234, 223, 123, 457, 124),
+(3, 1, 'dfdsfd113iebn', '2018-01-18', '2000-02-12', 324, 123, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 456, 754, 232, 454, 113, 200, 223, 123, 457, 546),
+(4, 1, 'ef234qr4f', '2018-01-18', '2000-02-12', 10, 45, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 100, 110, 110, 110, 113, 200, 223, 123, 457, 342),
+(5, 1, '4rt43f13iebn', '2018-01-18', '2000-02-12', 565, 30, 545, 100, 110, 321, 110, 234, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 120, 110, 110, 343, 113, 234, 223, 123, 457, 124),
+(6, 1, 'dfddefqef113iebn', '2017-12-28', '2000-02-12', 324, 123, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 456, 754, 232, 454, 113, 200, 223, 123, 457, 546),
+(7, 1, 'farw34wt3', '2018-01-18', '2000-02-12', 10, 45, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 100, 110, 110, 110, 113, 200, 223, 123, 457, 342),
+(8, 1, 'fa4t43fq4r', '2017-12-04', '2000-02-12', 565, 30, 545, 100, 110, 321, 110, 234, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 120, 110, 110, 343, 113, 234, 223, 123, 457, 124),
+(9, 1, 'dfdsfd113iebn', '2017-12-10', '2000-02-12', 324, 123, 300, 100, 110, 110, 110, 113, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 456, 754, 232, 454, 113, 200, 223, 123, 457, 546),
+(10, 1, 'd4qefawf', '2018-01-18', '2000-02-12', 10, 45, 300, 123, 12123, 123, 343, 343, 123, 223, 123, 451, 213, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 100, 110, 110, 110, 113, 200, 223, 123, 457, 342),
+(11, 1, 'dffd1afefa13iebn', '2018-01-12', '2000-02-12', 565, 30, 545, 123, 123, 435, 656, 234, 200, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 120, 110, 110, 343, 113, 234, 223, 123, 457, 124),
+(12, 1, 'dffdcefaw113iebn', '2017-12-08', '2000-02-12', 324, 656, 300, 6562, 1231, 1231, 1232, 222, 123, 223, 123, 451, 300, 100, 110, 110, 110, 113, 200, 223, 123, 452, 300, 456, 754, 232, 454, 113, 200, 223, 123, 457, 546),
+(13, 1, 'dffdcefaw113iebn', '2017-12-08', '2000-02-12', 324, 656, 300, 6562, 1231, 1231, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -59,17 +105,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `Dni` char(9) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Dni` (`Dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Restricciones para tablas volcadas
+-- Volcado de datos para la tabla `user`
 --
 
---
--- Filtros para la tabla `description`
---
-ALTER TABLE `description`
-  ADD CONSTRAINT `description_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+INSERT INTO `user` (`Id`, `Name`, `Surname`, `Dni`) VALUES
+(1, 'Medico1', 'Psy', '33223133R');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
